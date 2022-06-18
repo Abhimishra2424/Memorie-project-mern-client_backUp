f112dba2768bc1dfed55109bde03b4ffc442a99a
Abhishek Mishra <abmishra700@gmail.com>
2022-06-18 22:28:43 +0530

    APP define

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React from "react";
 
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import memories from "./images/memories.png";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="primary">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          src={memories ? memories : "https://i.imgur.com/XqQXQ.png"}
+          alt="memories"
+          height={60}
+        />
+      </AppBar>
+      <Grow in>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form />
+          </Grid>
+        </Grid>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./post/Post";
+
+const Posts = () => {
+  return (
+    <React.Fragment>
+      <Post />
+      <Post />
+    </React.Fragment>
+  );
+};
+
+export default Posts;
