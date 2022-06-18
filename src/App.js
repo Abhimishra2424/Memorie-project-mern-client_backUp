c666e8149dcffd093253ba0c0ed4fafad1c28046
Abhishek Mishra <abmishra700@gmail.com>
2022-06-18 23:43:24 +0530

    base setup useSelector

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./post/Post";
 
+import useStyles from "./styles";
+
 const Posts = () => {
+  // eslint-disable-next-line
+  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+
+  console.log(posts);
+
   return (
     <React.Fragment>
       <Post />
