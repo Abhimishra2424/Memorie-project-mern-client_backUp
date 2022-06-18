6f9e7b2637b81d55219cd4734d5d9df75d2bd918
Abhishek Mishra <abmishra700@gmail.com>
2022-06-18 23:31:21 +0530

    Action Creators

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
+import { getPosts } from "./actions/posts";
+
 import memories from "./images/memories.png";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
+
+import { useDispatch } from "react-redux";
 
 import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxWidth="lg">
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,7 @@
 export default (posts = [], action) => {
   switch (action.type) {
-    case "FETCH_POSTS":
-      return [...posts];
+    case "FETCH_ALL":
+      return action.payload;
     case "CREATE_POST":
       return [...posts];
     default:
